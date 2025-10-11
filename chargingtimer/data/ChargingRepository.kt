d2b17b4efda4aa98115c7e58ebdbe30e50260c64
Alexandr Gavrishev <algavris@microsoft.com>
2025-10-11 22:21:15 +0300

    cleanup unused files

--- chargingtimer/data/ChargingRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.anod.chargingtimer.data
-
-interface ChargingRepository {
-    fun saveSettings(settings: ChargingSettings)
-    fun loadSettings(onLoaded: (ChargingSettings) -> Unit)
-}
-
-expect class ChargingRepositoryImpl() : ChargingRepository
